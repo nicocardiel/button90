f929d904d501fdb417bf518f9e55417d1901620f
Nicolás Cardiel <cardiel@ucm.es>
2025-03-21 21:22:44 +0100

    Preliminary version with some plots

--- src/pgbutt_rpgfunctions.f90
+++ src/pgbutt_rpgfunctions.f90
@@ -1,4 +1,5 @@
 module pgbutt_rpgfunctions
+    use pgbutt_viewport
     implicit none
 
 contains
@@ -47,5 +48,66 @@ contains
         call pgscr(15,0.6,0.6,0.6) ! almost intermediate gray
 
     end subroutine rpgbegin
+
+    !--------------------------------------------------------------------------
+    subroutine rpgenv(viewport_ptr, xmin, xmax, ymin, ymax, just, axis)
+        implicit none
+        type(Viewport), pointer, intent(in) :: viewport_ptr
+        real, intent(in) :: xmin, xmax, ymin, ymax
+        integer, intent(in) :: just, axis
+
+        real :: x1v, x2v, y1v, y2v
+        character(len=10) :: xopts, yopts
+
+        if(xmin.eq.xmax)then
+            write(*, '(a)')'Invalid x limits in rpgenv: xmin = xmax.'
+            return
+        else if(ymin.eq.ymax)then
+            write(*, '(a)')'Invalid y limits in rpgenv: ymin = ymax.'
+            return
+        end if
+
+        x1v = viewport_ptr%x1v
+        x2v = viewport_ptr%x2v
+        y1v = viewport_ptr%y1v
+        y2v = viewport_ptr%y2v
+
+        call pgvport(x1v, x2v, y1v, y2v)
+
+        if(just.eq.1)then
+            call pgwnad(xmin,xmax,ymin,ymax)
+        else
+            call pgswin(xmin,xmax,ymin,ymax)
+        end if
+        !
+        yopts='*'
+        if(axis.eq.-2)then
+            xopts= char(32)
+        else if(axis.eq.-1)then
+            xopts='bc'
+        else if(axis.eq.0)then
+            xopts='bcnst'
+        else if(axis.eq.1)then
+            xopts='abcnst'
+        else if(axis.eq.2)then
+            xopts='abcgnst'
+        else if(axis.eq.10)then
+            xopts='bcnstl'
+            yopts='bcnst'
+        else if(axis.eq.20)then
+            xopts='bcnst'
+            yopts='bcnstl'
+        else if(axis.eq.30)then
+            xopts='bcnstl'
+            yopts='bcnstl'
+        else
+            write(*,'(a)')'rpgenv: illegal axis argument.'
+            xopts='bcnst'
+        end if
+        if(yopts.eq.'*') yopts=xopts
+        ! draw box
+        call pgbox(xopts,0.0,0,yopts,0.0,0)
+
+    end subroutine rpgenv
 
 end module pgbutt_rpgfunctions
--- src/samplebutton.f90
+++ src/samplebutton.f90
@@ -4,47 +4,26 @@ program samplebutton
     implicit none
 
     type(CanvasDef) :: canvas
-    type(Viewport), pointer :: viewport1, viewport2, viewport3
+    type(Viewport), pointer :: viewport1, viewport2
     type(Viewport), pointer :: viewport_ptr
     integer :: i
     integer :: nb
     real :: xc, yc
+    real :: xx(100), yy(100)
     character(len=1) ch
-    character(len=255) cdummy
     logical :: verbose = .true.
 
     !open graphics output
     call rpgbegin(verbose_=verbose)
 
-    call canvas%add_button_viewport(0.00, 1.00, 0.90, 1.00, 6, 1, viewport1)
-    call canvas%add_button_viewport(0.00, 1.00, 0.68, 0.88, 6, 2, viewport2)
-    call canvas%add_plot_viewport(0.00, 1.00, 0.00, 0.65, viewport3)
+    call canvas%add_button_viewport(0.05, 0.95, 0.80, 0.95, 6, 2, viewport1)
+    call canvas%add_plot_viewport(0.10, 0.95, 0.10, 0.70, viewport2)
 
-    if (verbose) then
-        print *, canvas%list_button_viewports%count
-        print *, canvas%list_plot_viewports%count
-        call pgsci(2)
-        call viewport1%plot_boundary()
-        call viewport1%print_corners()
-        call pgsci(3)
-        call viewport2%plot_boundary()
-        call viewport2%print_corners()
-        call pgsci(4)
-        call viewport3%plot_boundary()
-        call viewport3%print_corners()
-        call pgsci(1)
-    end if
+    call viewport1%button(1, 'sin', 0)
+    call viewport1%button(2, 'cos', 0)
+    call viewport1%button(6, 'EXIT', 0)
 
-    do i = 1, size(viewport1%button_array)
-        write(cdummy, '(a5,i2.2)') 'test_', i
-        call viewport1%button(i, trim(cdummy), 0)
-    end do
-
-    do i = 1, size(viewport2%button_array)
-        write(cdummy, '(a5,i2.2)') 'test_', i
-        call viewport2%button(i, trim(cdummy), 0)
-    end do
-
+    ! main loop
     do
         call pgcurs(xc, yc, ch)
         call canvas%get_viewport_from_mouse(xc, yc, .true., viewport_ptr)
@@ -52,11 +31,45 @@ program samplebutton
             print *, 'Found #', viewport_ptr%number
             print *, associated(viewport_ptr, viewport1)
             print *, associated(viewport_ptr, viewport2)
-            print *, associated(viewport_ptr, viewport3)
             call viewport_ptr%ifbutton(xc, yc, nb)
             print *, 'nb=', nb
         end if
-        if (ch .eq. 'X') exit
+        
+        !---
+        if (nb.eq.0) then
+            write(*, '(a,$)') 'Cursor at:'
+            write(*, *) xc, yc
+        !---
+        elseif (nb.eq.1) then
+            call viewport1%button(nb, 'sin', 5)
+            do i = 1, 100
+                xx(i) = real(i-1)/99
+                yy(i) = sin(xx(i) * 2 * 3.141593)
+            end do
+            call rpgenv(viewport2, 0., 1., -1.1, 1.1, 0, 0)
+            call pgline(100, xx, yy)
+            call pgvport(0.,1.,0.,1.)
+            call pgwindow(0.,1.,0.,1.)
+            call viewport1%button(nb, 'sin', 0)
+        !---
+        elseif (nb.eq.2) then
+            call viewport1%button(nb, 'cos', 5)
+            do i = 1, 100
+                xx(i) = real(i-1)/99
+                yy(i) = cos(xx(i) * 2 * 3.141593)
+            end do
+            call rpgenv(viewport2, 0., 1., -1.1, 1.1, 0, 0)
+            call pgline(100, xx, yy)
+            call pgvport(0.,1.,0.,1.)
+            call pgwindow(0.,1.,0.,1.)
+            call viewport1%button(nb, 'cos', 0)
+        !---
+        elseif (nb.eq.6) then
+            call viewport1%button(6, 'EXIT', 5)
+            write(*, '(a,$)') 'Press <RETURN> to EXIT'
+            read(*, *)
+            exit
+        end if
     end do
 
     !end of program
